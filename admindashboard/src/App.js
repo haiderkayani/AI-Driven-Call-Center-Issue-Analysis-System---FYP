da62a63dca5b860fa58684fbf47bea9fa4b0f2ad
la1baa <laibashafqat42@gmail.com>
2025-03-07 11:06:27 +0500

    Added Pie Chart

--- admindashboard/src/App.js
+++ admindashboard/src/App.js
@@ -11,7 +11,7 @@ import Form from "./scenes/form"
 // import Line from "./scenes/line"
 // import FAQ from "./scenes/faq"
 import Bar from "./scenes/bar"
-// import Pie from "./scenes/pie"
+import Pie from "./scenes/pie"
 // import Geography from "./scenes/geography"
 // import Calendar from "./scenes/calendar"
 
@@ -35,7 +35,7 @@ function App() {
               {/* <Route path='/line' element={<Line />} /> */}
               {/* <Route path='/faq' element={<FAQ />} /> */}
               <Route path='/bar' element={<Bar />} />
-              {/* <Route path='/pie' element={<Pie />} /> */}
+              <Route path='/pie' element={<Pie />} />
               {/* <Route path='/geography' element={<Geography />} /> */}
               {/* <Route path='/calendar' element={<Calendar />} /> */}
             </Routes>
